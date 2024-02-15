e1e32efe771c105094a1a602261e0a5ebad7fcd5
Michael Garbosa <garbosamichael1@gmail.com>
2024-02-15 18:05:52 +0800

    Refactoring comments

--- src/ageRange.java
+++ src/ageRange.java
@@ -8,7 +8,7 @@ public class ageRange {
         int max;
 
         if(age<=14){
-            System.out.println("Dont date yet");
+            System.out.println("Don't date yet");
         } else {
             min = (age / 2 + 7);
             max1 = (age - 7);
@@ -20,5 +20,5 @@ public class ageRange {
 }
 
 //age = 27   =>   20-40
-//age = 5    =>   4-5
+//age = 5    =>   Cant date yet
 //age = 17   =>   15-20
